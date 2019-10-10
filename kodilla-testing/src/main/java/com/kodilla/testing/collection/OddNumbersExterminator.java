9ff8e03f9d14896715a33258abaa95df51b61ed3
patpaw <pawlik.patr@gmail.com>
2019-10-10 11:32:45 +0200

    collection

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -0,0 +1,31 @@
+package com.kodilla.testing.collection;
+
+
+public class OddNumbersExterminator {
+
+    public int exterminate(ArrayList<Integer> numbers) {
+    return ArrayList<Integer> numbersEven);
+    }
+
+
+}
+/*
+class IntegerSplitter {
+    private List<Integer> numbersOdd = new ArrayList<>();
+    private List<Integer> numbersEven = new ArrayList<>();
+
+    public void splitArray(List<Intger> numbers) {
+        while(numbers.size() > 0) {
+            Integer s = numbers.get(i);
+            if(s.size() % 2 == 0) {
+                even.add(s);
+            } else {
+                odd.add(s);
+            }
+        }
+        System.out.println(numbersOdd);
+        System.out.println();
+        System.out.println(numbersEven);
+    }
+}
+*/
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,59 @@
+package com.kodilla.testing.collection;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class CollectionTestSuite {
+    @Before
+    public void before(){
+        System.out.println("Test Case: begin testOddNumbersExterminatorEmptyList && testOddNumbersExterminatorNormalList");
+    }
+    @After
+    public void after(){
+        System.out.println("Test Case: end testOddNumbersExterminatorEmptyList && testOddNumbersExterminatorNormalList");
+    }
+    @Test
+    public void testOddNumbersExterminatorEmptyList() {
+        OddNumbersExterminator numbersExterminator = new OddNumbersExterminator(){
+           private List<Integer> numbers = new ArrayList<>();
+            numbers.add(0);
+            numbers.add(1);
+            numbers.add(2);
+            numbers.add(3);
+            numbers.add(4);
+            numbers.add(5);
+            numbers.add(6);
+            numbers.add(7);
+            numbers.add(8);
+            numbers.add(9);
+            numbers.add(10);
+
+            int numbersList = numbers;
+        }
+
+            private List<Integer> numbersOdd = new ArrayList<>();
+            private List<Integer> numbersEven = new ArrayList<>();
+
+            public void splitArray(List<Integer> numbers) {
+                while(numbers.size() > 0) {
+                    Integer s = numbers.get(i);
+                    if(s.size() % 2 == 0) {
+                        even.add(s);
+                    } else {
+                        odd.add(s);
+                    }
+                }
+                System.out.println(numbersOdd);
+                System.out.println();
+                System.out.println(numbersEven);
+            }
+
+    }
+    @Test
+    public void testOddNumbersExterminatorNormalList() {
+
+    }
+}
