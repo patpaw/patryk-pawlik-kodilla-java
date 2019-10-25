558c100171041b7cf54a037c51e57e666d8602c0
patpaw <pawlik.patr@gmail.com>
2019-10-25 11:56:19 +0200

    IntStream Ex. 7.5

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,5 +1,29 @@
 package com.kodilla.stream.array;
 
-public class ArrayOperationsTestSuite {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class ArrayOperationsTestSuite {
+    @Test
+    public void testGetAverage() {
+        //Given
+        int[] array = new int[10];
+        array[0] = 4;
+        array[1] = 2;
+        array[2] = 2;
+        array[3] = 8;
+        array[4] = 6;
+        array[5] = 2;
+        array[6] = 5;
+        array[7] = 1;
+        array[8] = 3;
+        array[9] = 9;
+
+        //When
+        double average1 = ArrayOperations.getAverage(array);
+        System.out.println("\n" + "średnia: " + average1);
+
+        //Then
+        Assert.assertEquals(4.2, average1, 0.01);
+    }
 }
